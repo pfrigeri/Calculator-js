f3c7622f8a84f1a42ad30d94767425a5a34f8f3f
pfrigeri <asockmf@gmail.com>
2025-10-07 20:28:28 -0300

    Add an initial interface for the calculator and the operate function

--- script.js
+++ script.js
@@ -15,16 +15,36 @@ const multiply = (num1, num2) => {
 }
 
 const divide = (num1, num2) => {
-    if(num1 == 0 && num2 == 0) return "Undefined"    
+    if(num1 == 0 && num2 == 0) return "Undefined"; 
     else if(num1 == 0 && num2 != 0) return 0;
-    else if(num1 != 0 && num2 == 0) return Infinity
+    else if(num1 != 0 && num2 == 0) return Infinity;
     
     return num1 / num2;
 }
+
 //global variables
 let number1 = 0;
 let operator = '';
 let number2 = 0;
 
+const operate = (operator, num1, num2) => {
+    switch(operator){
+        case '+':
+            add(num1,num2);
+            break;
+        case '-':
+            subtract(num1,num2);
+            break;
+        case '*' || "x":
+            multiply(num1,num2);
+            break;
+        case '/':
+            divide(num1,num2);
+            break;
+        default:
+            alert("Choose a valid operator !")
+            break;
+    }
+}
 
 console.log(divide(0,0));
